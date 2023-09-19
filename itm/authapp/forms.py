230f537935096b88ca11f721d8a9d0ba0c1c9a71
fanfat7777777 <93639218+fanfat7777777@users.noreply.github.com>
2023-09-19 11:56:58 +0300

    Adding registration
    Adding registration
    
    ---------
    
    Co-authored-by: fanfat <fanfat@fanfat.ru>

--- itm/authapp/forms.py
+++ itm/authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
 
 
@@ -15,3 +15,86 @@ class LoginUserForm(AuthenticationForm):
                     'class': 'form-control mb-4',
                     'placeholder': 'Введите пароль'
                 }))
+    
+    
+
+class CustomUserCreationForm(UserCreationForm):
+
+    username = forms.CharField(label='Логин*', widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Введите свой Логин'
+                }))
+    
+    first_name = forms.CharField(label='Имя', widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Введите своё Имя'
+                }),
+                # Поле не обязательно
+                required=False
+                )
+    
+    last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Введите свою Фамилию'
+                }),
+                # Поле не обязательно
+                required=False
+                )
+    
+    birthday = forms.CharField(label='День рождения*', widget=forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Введите День рождения',
+                }))
+    
+    email = forms.CharField(label='Email*', widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Укажите Электронную почту',
+                }))
+    
+    phone = forms.CharField(label='Телефон', widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': '+7(999)999-99-99',
+                }),
+                # Поле не обязательно
+                required=False
+                )
+    
+    password1 = forms.CharField(label='Пароль*', widget=forms.PasswordInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Введите пароль'
+                }))
+    
+    password2 = forms.CharField(label='Повторите пароль*', widget=forms.PasswordInput(
+                attrs={
+                    'class': 'form-control mb-4',
+                    'placeholder': 'Повторите пароль'
+                }))
+    
+    submit = forms.CharField(label='', widget=forms.TextInput(
+                attrs={
+                    'type': 'submit',
+                    'class': 'form-control mb-4 w-50 mx-auto',
+                    'value': 'Зарегистрироваться'
+                }))
+    
+    # Очерёдность полей формы
+    field_order = [
+        'username', 
+        'first_name', 
+        'last_name', 
+        'phone', 
+        'birthday', 
+        'email',  
+        'password1', 
+        'password2', 
+        'submit'
+        ]
+    
--- itm/authapp/views.py
+++ itm/authapp/views.py
@@ -1,10 +1,40 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
-from authapp.forms import LoginUserForm
+from django.views.generic import TemplateView, CreateView
+from authapp.forms import LoginUserForm, CustomUserCreationForm
+from django.urls import reverse_lazy
+from authapp import models
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth.password_validation import validate_password
 
 
-class RegPageView(TemplateView):
+class RegPageView(CreateView):
     template_name = 'mainapp/register.html'
+    form_class = CustomUserCreationForm
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if all(
+                (
+                    request.POST.get("username"),
+                    request.POST.get("email"),
+                    request.POST.get("password1"),
+                    validate_password(request.POST.get("password1")) == None,
+                    request.POST.get("password1") == request.POST.get("password2"),
+                )
+            ):
+                new_user = models.CustomUser.objects.create(
+                    username=request.POST.get("username"),
+                    first_name=request.POST.get("first_name"),
+                    last_name=request.POST.get("last_name"),
+                    birthday=request.POST.get("birthday"),
+                    email=request.POST.get("email"),
+                    phone=request.POST.get("phone"),
+                )
+                new_user.set_password(request.POST.get("password1"))
+                new_user.save()
+                return HttpResponseRedirect(reverse_lazy("auth"))
+        except Exception as exp:
+            return HttpResponseRedirect(reverse_lazy("registration"))
 
 
 class AuthPageView(LoginView):
