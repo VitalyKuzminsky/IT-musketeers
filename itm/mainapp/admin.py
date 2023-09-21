921a05b34d14097eb9e5fff21dd4e95bcbc2680a
MarselMi <marselmihajlov@yandex.ru>
2023-09-21 19:15:49 +0500

    admin_

--- itm/mainapp/admin.py
+++ itm/mainapp/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from authapp.models import CustomUser
+from mainapp.models import (
+    Category, Services, Reviews, Basket
+)
 
 
 @admin.register(CustomUser)
 class AdminCustomUser(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Services)
+class AdminServices(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reviews)
+class AdminReviews(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Basket)
+class AdminBasket(admin.ModelAdmin):
     pass
--- itm/mainapp/data_library.py
+++ itm/mainapp/data_library.py
@@ -1,6 +1,7 @@
 import pymysql
 from itm import settings
 
+
 def get_users_information():
     conn = pymysql.connect(host=settings.DATABASES.get('default').get('HOST'),
                            user=settings.DATABASES.get('default').get('USER'),
@@ -12,16 +13,16 @@ def get_users_information():
     try:
         with conn.cursor() as cursor:
             response = f'''
-            SELECT 
-                `cu`.`id`, 
-                `cu`.`username`, 
-                `cu`.`first_name`, 
-                `cu`.`last_name`, 
-                `cu`.`is_staff`, 
-                `cu`.`is_superuser`, 
-                (SELECT GROUP_CONCAT(`bh`.`organization_holder`) FROM `mainapp_balanceholder` bh 
-                WHERE `bh`.`id` IN (SELECT `ah`.`balanceholder_id` FROM `mainapp_customuser_available_holders` ah 
-                                    WHERE `ah`.`customuser_id` = `cu`.`id`)) AS 'balanceholder_id' 
+            SELECT
+                `cu`.`id`,
+                `cu`.`username`,
+                `cu`.`first_name`,
+                `cu`.`last_name`,
+                `cu`.`is_staff`,
+                `cu`.`is_superuser`,
+                (SELECT GROUP_CONCAT(`bh`.`organization_holder`) FROM `mainapp_balanceholder` bh
+                WHERE `bh`.`id` IN (SELECT `ah`.`balanceholder_id` FROM `mainapp_customuser_available_holders` ah
+                                    WHERE `ah`.`customuser_id` = `cu`.`id`)) AS 'balanceholder_id'
             FROM `mainapp_customuser` cu
             '''
             cursor.execute(response)
