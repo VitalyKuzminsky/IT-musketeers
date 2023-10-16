642bf0d693d6f56cc5cf483791a5cb22174816c9
MarselMi <marselmihajlov@yandex.ru>
2023-10-16 17:53:35 +0500

    услуги_и_категории

--- itm/mainapp/urls.py
+++ itm/mainapp/urls.py
@@ -14,21 +14,21 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
 
     path('web-development/', WebPageView.as_view(), name='web'),
-    path('web-development/<int:pk>/detail/', WebDetailPageView.as_view(), name='web_detail'),
-
     path('mobile-development/', MobilPageView.as_view(), name='mobile'),
-    path('mobile-development/<int:pk>/detail/', MobilDetailPageView.as_view(), name='mobile_detail'),
-
     path('marketing/', MarketingPageView.as_view(), name='marketing'),
-    path('marketing/<int:pk>/detail/', MarketingDetailPageView.as_view(), name='marketing_detail'),
+
+    path('service/<int:pk>/detail/', ServiceDetailPageView.as_view(), name='service_detail'),
 
     path('order/', OrderPageView.as_view(), name='order'),
     path('profile/', ProfilePageView.as_view(), name='profile'),
     path('review/<int:pk>/', ReviewPageView.as_view(), name='review'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
 
     # path('design/', DesignPageView.as_view(), name='design'),
 
-    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('web-development/<int:pk>/detail/', MarketingDetailPageView.as_view(), name='web_detail'),
+    path('mobile-development/<int:pk>/detail/', MobilDetailPageView.as_view(), name='mobile_detail'),
+    path('marketing/<int:pk>/detail/', MarketingDetailPageView.as_view(), name='marketing_detail'),
 ]
 
 if settings.DEBUG:
--- itm/mainapp/views.py
+++ itm/mainapp/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from mainapp.models import *
 
 
@@ -18,27 +19,50 @@ class MainPageView(generic.ListView):
 
 
 class WebPageView(TemplateView):
+    model = Category.objects.filter(name='Web-Разработка')
+    extra_context = {'title': model[0]}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Services.objects.filter(category_id=Category.objects.filter(name='Web-Разработка')[0])
+        context['category'] = Category.objects.filter(name='Web-Разработка')
+        return context
+
     template_name = 'mainapp/web.html'
 
 
-class WebDetailPageView(TemplateView):
-    template_name = 'mainapp/web_detail.html'
-
-
 class MobilPageView(TemplateView):
+    model = Category.objects.filter(name='Мобильная Разработка')
+    extra_context = {'title': model[0]}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Services.objects.filter(category_id=Category.objects.filter(name='Мобильная Разработка')[0])
+        context['category'] = Category.objects.filter(name='Мобильная Разработка')
+        return context
     template_name = 'mainapp/mobile.html'
 
 
-class MobilDetailPageView(TemplateView):
-    template_name = 'mainapp/mobile_detail.html'
-
-
 class MarketingPageView(TemplateView):
+    model = Category.objects.filter(name='Маркетинг')
+    extra_context = {'title': model[0]}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Services.objects.filter(category_id=Category.objects.filter(name='Маркетинг')[0])
+        context['category'] = Category.objects.filter(name='Маркетинг')
+        return context
     template_name = 'mainapp/marketing.html'
 
 
-class MarketingDetailPageView(TemplateView):
-    template_name = 'mainapp/marketing_detail.html'
+class ServiceDetailPageView(DetailView):
+    template_name = 'mainapp/detail.html'
+    model = Services
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['service'] = get_object_or_404(Services, pk=self.kwargs.get('pk'))
+        return context_data
 
 
 class ContactsPageView(TemplateView):
@@ -51,6 +75,14 @@ class OrderPageView(TemplateView):
 
 class ReviewPageView(TemplateView):
     template_name = 'mainapp/review.html'
+
+
+class MobilDetailPageView(TemplateView):
+    template_name = 'mainapp/mobile_detail.html'
+
+
+class MarketingDetailPageView(TemplateView):
+    template_name = 'mainapp/marketing_detail.html'
 
 
 # class DesignPageView(TemplateView):
