83f1636eabeeda1772790d3e81f653eb943dd754
fanfat <fanfat@fanfat.ru>
2023-10-19 20:12:08 +0300

    Страница profile
    Вывод данных на страницу
    Правка шаблона страницы
    Добавление файлов стилей

--- itm/authapp/views.py
+++ itm/authapp/views.py
@@ -3,6 +3,10 @@ from django.views.generic import TemplateView, CreateView
 from authapp.forms import LoginUserForm, CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.utils import dateformat
+from mainapp.models import Basket
 
 
 class RegPageView(CreateView):
@@ -21,6 +25,28 @@ class CustomLogoutView(LogoutView):
     pass
 
 
-class ProfilePageView(TemplateView):
+class ProfilePageView(LoginRequiredMixin, TemplateView):
     template_name = 'mainapp/profile.html'
+
+    def get_queryset(self):
+        some_param = self.request.user.pk  # Здесь может быть ваш параметр
+        return Basket.objects.filter(custom_user_id=some_param)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filtered_objects'] = self.get_queryset()
+        context['user_name'] = self.request.user.username
+        return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     #Получаем текущего пользователя
+    #     user_pk = self.request.user.pk
+
+    #     filtered_objects = Basket.objects.filter(custom_user_id=user_pk)
+        
+    #     context['filtered_objects'] = filtered_objects
+
+    #     return context
+
 
--- itm/itm/settings.py
+++ itm/itm/settings.py
@@ -118,6 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Формат вывода даты
+DATE_FORMAT = 'd E Y'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
